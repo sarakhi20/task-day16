568ffc56f1634ff677298a3f77676481d014e120
sarakhi20 <sarakhiammu96@gmail.com>
2024-08-28 13:10:32 +0530

    first commit

---
+++ callback.js
@@ -0,0 +1,35 @@
+let count = 10;
+
+setTimeout(() => {
+  document.body.innerHTML = count--;
+  setTimeout(() => {
+    document.body.innerHTML = count--;
+    setTimeout(() => {
+      document.body.innerHTML = count--;
+      setTimeout(() => {
+        document.body.innerHTML = count--;
+        setTimeout(() => {
+          document.body.innerHTML = count--;
+          setTimeout(() => {
+            document.body.innerHTML = count--;
+            setTimeout(() => {
+              document.body.innerHTML = count--;
+              setTimeout(() => {
+                document.body.innerHTML = count--;
+                setTimeout(() => {
+                    document.body.innerHTML = count--;
+                    setTimeout(() => {
+                        document.body.innerHTML = count--;
+                setTimeout(() => {
+                  document.body.innerHTML = "Happy Independence Day!";
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
